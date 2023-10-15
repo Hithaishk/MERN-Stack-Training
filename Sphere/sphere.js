2df46763fa8414a4f8b567b2e0f00e7f1c3fd67b
Hithaish <hithaishkaramber@gmail.com>
2023-10-15 14:40:00 +0530

    sphere

---
+++ Sphere/sphere.js
@@ -0,0 +1,9 @@
+function c(){
+    let radius=document.querySelector("#n1").value
+    let res;
+    const pi=(22/7);
+    res=(4/3)*pi*(radius**3);
+    document.querySelector("#output").innerHTML="Volume is :  "+res;
+    // document.querySelector("#res").innerHTML=res
+    
+}
