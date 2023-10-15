bd43e1b0fc35c38d22e050edcf30ee0bab7eb137
Hithaish <hithaishkaramber@gmail.com>
2023-10-15 14:49:35 +0530

    sphere

--- Sphere/sphere.js
+++ Sphere/sphere.js
@@ -3,6 +3,7 @@ function c(){
     let res;
     const pi=(22/7);
     res=(4/3)*pi*(radius**3);
+    // res=math.Round((4/3)*pi*(radius**3));
     document.querySelector("#output").innerHTML="Volume is :  "+res;
     // document.querySelector("#res").innerHTML=res
     
